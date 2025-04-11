6065dd5bb8ad4e1e1cc80dc1a886b63ce0c5dd36
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-04-11 12:34:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 # Load from env or set directly here
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7966519826:AAFzU7F_l7jNVk3fybpt8hl5Yj4zg4q3VOY")
 
 # MongoDB URI (change this for production)
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://AOMusic:AOMusic@cluster0.sibxiqk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Default map size (rows x cols)
 MAP_WIDTH = 5
